c04082362d2d1abc2fa6a869604f9a36e49499b7
Nele <s0550233@htw-berlin.de>
2017-01-04 13:30:44 +0100

    add changeColor()

--- src/lamp/Lamp.java
+++ src/lamp/Lamp.java
@@ -1,15 +1,18 @@
 package lamp;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
 
 import buttonLampInterfaces.LampInterface;
 
-public class Lamp extends java.rmi.server.UnicastRemoteObject implements buttonLampInterfaces.LampInterface  {
+public class Lamp extends java.rmi.server.UnicastRemoteObject implements ActionListener, buttonLampInterfaces.LampInterface  {
 	
 	boolean status = false;
-	
+	LampUI lUi;
 	public Lamp() throws RemoteException {
 		super();
+		lUi= new LampUI();
 	}
 
 
@@ -17,11 +20,20 @@ public class Lamp extends java.rmi.server.UnicastRemoteObject implements buttonL
 	public void changeStatus() throws RemoteException {
 		if (status) {
 			status = false;
+			lUi.changeColor();
 		}
 		else {
 			status = true;
+			lUi.changeColor();
 		}
 		System.out.println("Lamp is on: " + status);
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/lamp/LampUI.java
+++ src/lamp/LampUI.java
@@ -2,29 +2,53 @@ package lamp;
 
 import java.awt.Color;
 import java.awt.Event;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class LampUI {
+public class LampUI implements ActionListener{
+	JPanel p;
+	Boolean onOff = false;
 	
-	//Methode zum ändern der Farbe
-	public void press(Event e){
+	public LampUI(){
 		
-	}
-	
-	
-	public static void main( String[] args )
-	  {
 	    JFrame f = new JFrame( "Lampe" );
 	    f.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	    f.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 	    //f.add( new JLabel( String.format( "%tT", new Date() ) ) );
-	    JPanel p = new JPanel();
+	     p = new JPanel();
 	    p.setBackground(Color.BLACK);
 	    f.add(p);
 	    f.setVisible( true );
-	  }
+	}
+	
+
+	public void changeColor(){
+		 if (onOff) {
+			p.setBackground(Color.black);
+			onOff = false;
+		}else {
+			p.setBackground(Color.yellow);
+			onOff = true; 
+		}
+	}
+	
+	
+	/*public static void main( String[] args )
+	  {
+		new LampUI();
+	  }*/
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		this.changeColor();
+		
+	}
+
+
 
 
 }
