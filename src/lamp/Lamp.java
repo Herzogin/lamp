f1bd8ed410e52732fd51cf494be39c759e9bd5c8
MariamaB <candy91.mb@googlemail.com>
2017-01-25 14:16:51 +0100

    use Ibinder

--- src/lamp/Lamp.java
+++ src/lamp/Lamp.java
@@ -3,21 +3,26 @@ package lamp;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+
+import org.htw.fiw.vs.IBinder;
 
 public class Lamp extends java.rmi.server.UnicastRemoteObject implements ActionListener, buttonLampInterfaces.LampInterface  {
 	
 	boolean status = false;
 	LampUI lUi;
-	public Lamp() throws RemoteException, AlreadyBoundException, UnknownHostException {
+	public Lamp() throws RemoteException, AlreadyBoundException, UnknownHostException, MalformedURLException, NotBoundException {
 		super();
-		Registry registry = LocateRegistry.getRegistry(3000);
+		IBinder registry = (IBinder) Naming.lookup("rmi://141.45.210.50/binder");
 		
-		// TODO: bind to lamp/hostname/uID, we use currentTime as uID for developing locally
+		
 		registry.bind("lamp" +"/"+InetAddress.getLocalHost().getHostName()+"/"+ System.currentTimeMillis(), this);
 		lUi= new LampUI();
 		
@@ -42,7 +47,7 @@ public class Lamp extends java.rmi.server.UnicastRemoteObject implements ActionL
 		// TODO Auto-generated method stub
 	}
 	
-	public static void main(String[] args) throws RemoteException, AlreadyBoundException, UnknownHostException {
+	public static void main(String[] args) throws RemoteException, AlreadyBoundException, UnknownHostException, MalformedURLException, NotBoundException {
 		new Lamp();
 	}
 }
